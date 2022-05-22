b9ad54b158a54da33008e4648286d7ae61102cd8
johnsnoa <johnson.noah.b@gmail.com>
2022-05-22 13:38:03 -0500

    UIDEV-02 : Added classes and level entry

--- dnd-web.js
+++ dnd-web.js
@@ -1,13 +1,23 @@
 let $form = $('form');
+let new__level;
 
 $(function() {
-    $('.character__level').on('change', function () {
+    $('.character__level').on('change', new__level = function () {
+        let $last = $('.character__level:last');
+        
+        const lastLevel = $last[0];
         if ($('.character__level:last')[0].value !== '') {
-            let levels = $('.character__level').length;
-            $('.character__level:last')
+            $(`option[value="${$('.class-choices:last')[0].value}"]`).remove();
+            const classes = $('.character__level').length + 1;
+            let label = `<input type="text" list="classes" placeholder="Class" class="class-choices"/>`
+            $form.append(`<div class="input" id="class-${classes}"></div>`);
+            let $div = $('.input:last');
+            $div.append(label);
+            $last
                 .clone()
-                .appendTo('form')[0]
-                .value = '';
+                .appendTo($div)
+                .on('change', new__level);
+            $('.character__level:last')[0].value = '';
         }
 
     });
