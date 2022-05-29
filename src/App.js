530a51815bb6cc5f5540bfaa665aac45f6fdd074
Noah Johnson <NoahJohnson2@my.unt.edu>
2022-05-29 14:41:47 -0500

    FRONT-1 : Began refactoring to functional components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,62 @@
 import "./App.css";
 import CharacterForm from "./CharacterForm";
-import AbilitiesForm from "./AbilitiesForm";
-import { useEffect, Component } from "react";
-import $ from "jquery";
+// import AbilitiesForm from "./AbilitiesForm";
+import React, { useState } from "react";
+// import $ from "jquery";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+export default function App() {
+  const [characterInfo, setCharacterInfo] = useState({
+    info: { name: "", race: "" },
+  });
+  //const [characterInfoSet, markCharacterInfoSet] = useState(false);
 
-    this.getName = this.getName.bind(this);
-
-    this.state = {
-      character__name: "",
-    };
-  }
-
-  getName = (name) => {
-    this.setState({ character__name: name });
+  const handleCharacterInfo = (event, info) => {
+    setCharacterInfo({ info });
+    //markCharacterInfoSet(true);
+    event.preventDefault();
   };
 
-  abilitiesScreen = (e) => {
-    $(".character").removeClass("form__background--visible");
-    $(".abilities").addClass("form__background--visible");
-    e.preventDefault();
-  };
-
-  componentDidMount() {
-    $(".character").addClass("form__background--visible");
-  }
-  render() {
-    return (
-      <div className="App">
-        <AbilitiesForm />
-        <CharacterForm getName={this.getName} onSubmit={this.abilitiesScreen} />
-      </div>
-    );
-  }
+  return (
+    <div className="app">
+      <CharacterForm visibility={false} onSubmit={handleCharacterInfo} />
+      <p>
+        Your character is: {characterInfo.info["name"]}{" "}
+        {characterInfo.info["name"] && "the"} {characterInfo.info["race"]}
+      </p>
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.getName = this.getName.bind(this);
+
+//     this.state = {
+//       character__name: "",
+//     };
+//   }
+
+//   getName = (name) => {
+//     this.setState({ character__name: name });
+//   };
+
+//   abilitiesScreen = (e) => {
+//     $(".character").removeClass("form__background--visible");
+//     $(".abilities").addClass("form__background--visible");
+//     e.preventDefault();
+//   };
+
+//   componentDidMount() {
+//     $(".character").addClass("form__background--visible");
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <AbilitiesForm />
+//         <CharacterForm getName={this.getName} onSubmit={this.abilitiesScreen} />
+//       </div>
+//     );
+//   }
+// }
--- src/CharacterForm.js
+++ src/CharacterForm.js
@@ -1,63 +1,123 @@
 import React from "react";
 
-export default class CharacterForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      character__name: "",
-      character__class: "",
-    };
+const CharacterForm = (props) => {
+  return (
+    <form
+      className={`form__background character ${
+        !props.visibility && "form__background--visible"
+      }`}
+      onSubmit={(event) =>
+        props.onSubmit(event, {
+          name: event.target.character__name.value,
+          race: event.target.character__race.value,
+        })
+      }
+    >
+      <div className="form__box">
+        <label to="character__name">Character Name</label>
+        <input
+          type="text"
+          className="character__name"
+          name="character__name"
+          spellCheck="false"
+          required
+        />
+        <br className="form__break" />
+        <label to="character__race">Character Race</label>
+        <select
+          className="character__race"
+          name="character__race"
+          placeholder=""
+          required
+        >
+          <option value="">Select one</option>
+          <option value="Human">Human</option>
+          <option value="Aasimar">Aasimar</option>
+          <option value="Warforged">Warforged</option>
+          <option value="Yuan-ti">Yuan-Ti Pureblood</option>
+          <option value="Dwarf">Dwarf</option>
+          <option value="Triton">Triton</option>
+          <option value="Goliath">Goliath</option>
+          <option value="Tabaxi">tabaxi</option>
+          <option value="Dragonborn">Dragonborn</option>
+          <option value="Half-elf">Half-Elf</option>
+          <option value="Lizardfolk">Lizardfolk</option>
+          <option value="Gnome">Gnome</option>
+          <option value="Genasi">Genasi</option>
+          <option value="Aarakocra">Aarakocra</option>
+          <option value="Elf">Elf</option>
+          <option value="Tiefling">Tiefling</option>
+          <option value="Bugbear">Bugbear</option>
+          <option value="Kenku">Kenku</option>
+          <option value="Githyanki">Githyanki</option>
+          <option value="Tortle">Tortle</option>
+        </select>
+        <input type="submit" value="Next" />
+      </div>
+    </form>
+  );
+};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+export default CharacterForm;
 
-  handleChange(event) {
-    this.props.getName(event.target.value);
-  }
+// export default class CharacterForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       character__name: "",
+//       character__class: "",
+//     };
 
-  handleSubmit(event) {
-    alert(this.state.character__name);
-    event.preventDefault();
-  }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  render() {
-    return (
-      <form
-        className="form__background character"
-        onSubmit={this.props.onSubmit}
-      >
-        <div className="form__box">
-          <label to="character__name">Character Name</label>
-          <input
-            type="text"
-            className="character__name"
-            name="character__name"
-            value={this.props["character__name"]}
-            onChange={(event) => this.handleChange(event)}
-            spellCheck="false"
-          />
-          {/* <select
-            className="character__class"
-            name="character__class"
-            value={this.state["character__class"]}
-            onChange={this.handleChange}
-            required
-          >
-            <option value=""></option>
-            <option value="artificer">Artificer</option>
-            <option value="barbarian">Barbarian</option>
-            <option value="bard">Bard</option>
-            <option value="cleric">Cleric</option>
-            <option value="druid">Druid</option>
-            <option value="fighter">Fighter</option>
-            <option value="monk">Monk</option>
-            <option value="paladin">Paladin</option>
-            <option value="ranger">Ranger</option>
-          </select> */}
-          <input type="submit" value="Next" />
-        </div>
-      </form>
-    );
-  }
-}
+//   handleChange(event) {
+//     this.props.getName(event.target.value);
+//   }
+
+//   handleSubmit(event) {
+//     alert(this.state.character__name);
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <form
+//         className="form__background character"
+//         onSubmit={this.props.onSubmit}
+//       >
+//         <div className="form__box">
+//           <label to="character__name">Character Name</label>
+//           <input
+//             type="text"
+//             className="character__name"
+//             name="character__name"
+//             value={this.props["character__name"]}
+//             onChange={(event) => this.handleChange(event)}
+//             spellCheck="false"
+//           />
+//           {/* <select
+//             className="character__class"
+//             name="character__class"
+//             value={this.state["character__class"]}
+//             onChange={this.handleChange}
+//             required
+//           >
+//             <option value=""></option>
+//             <option value="artificer">Artificer</option>
+//             <option value="barbarian">Barbarian</option>
+//             <option value="bard">Bard</option>
+//             <option value="cleric">Cleric</option>
+//             <option value="druid">Druid</option>
+//             <option value="fighter">Fighter</option>
+//             <option value="monk">Monk</option>
+//             <option value="paladin">Paladin</option>
+//             <option value="ranger">Ranger</option>
+//           </select> */}
+//           <input type="submit" value="Next" />
+//         </div>
+//       </form>
+//     );
+//   }
+// }
