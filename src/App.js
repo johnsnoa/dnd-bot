b0399f5acf8256ada66f1c6572714fbe23dc68be
Noah Johnson <NoahJohnson2@my.unt.edu>
2022-06-05 13:55:48 -0500

    Stashed changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import "./App.css";
 import CharacterForm from "./CharacterForm";
-// import AbilitiesForm from "./AbilitiesForm";
+import LevelForm from "./LevelForm.jsx";
 import React, { useState } from "react";
-// import $ from "jquery";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   const [characterInfo, setCharacterInfo] = useState({
     info: { name: "", race: "" },
   });
-  //const [characterInfoSet, markCharacterInfoSet] = useState(false);
 
   const handleCharacterInfo = (event, info) => {
     setCharacterInfo({ info });
-    //markCharacterInfoSet(true);
     event.preventDefault();
   };
 
   return (
     <div className="app">
-      <CharacterForm visibility={false} onSubmit={handleCharacterInfo} />
+      <CharacterForm onSubmit={handleCharacterInfo} />
+      <LevelForm onSubmit={() => {}} />
       <p>
         Your character is: {characterInfo.info["name"]}{" "}
         {characterInfo.info["name"] && "the"} {characterInfo.info["race"]}
@@ -26,37 +25,3 @@ export default function App() {
     </div>
   );
 }
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.getName = this.getName.bind(this);
-
-//     this.state = {
-//       character__name: "",
-//     };
-//   }
-
-//   getName = (name) => {
-//     this.setState({ character__name: name });
-//   };
-
-//   abilitiesScreen = (e) => {
-//     $(".character").removeClass("form__background--visible");
-//     $(".abilities").addClass("form__background--visible");
-//     e.preventDefault();
-//   };
-
-//   componentDidMount() {
-//     $(".character").addClass("form__background--visible");
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <AbilitiesForm />
-//         <CharacterForm getName={this.getName} onSubmit={this.abilitiesScreen} />
-//       </div>
-//     );
-//   }
-// }
---
+++ src/LevelForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LevelForm = () => {
+
+};
+
+export default LevelForm;
